78c1087b57fb674fcb24292489922c4d6747cf93
Chloë Rivière <chloerivs@gmail.com>
2024-04-21 11:34:40 +0000

    Set up the checkAnswer function so it retrieves the user's answer from the text input box and returns a message based on whether it is correct or not.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for(let button of buttons) {
         button.addEventListener('click', function() {
             if(this.getAttribute('data-type') === 'submit') {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute('data-type');
                 runGame(gameType);
@@ -32,10 +32,23 @@ function runGame(gameType) {
 }
 
 function checkAnswer() {
+    //Fetches the user's answer from the input box
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
 
+    //Checks the user's answer against the correct answer
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+    
+    if(isCorrect) {
+        alert('Hey! You got it right! :D');
+    } else {
+        alert(`Aww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
-/*Retrieves the operands and operator from the dom and returns correct answer*/
+/*Retrieves the operands and operator from the DOM and returns correct answer*/
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById('operand1').innerText);
     let operand2 = parseInt(document.getElementById('operand2').innerText);
